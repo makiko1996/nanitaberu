864be0d2034eb589f56d51b3b8b2a5dc9c85f49c
Makiko1996 <t_makiko_1996@yahoo.co.jp>
2022-02-02 05:58:41 +0000

    [fix]top&show_url

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,21 +1,25 @@
 class Public::HomesController < ApplicationController
-    
+
   def top
-    
+
     # 新着投稿の表示
     @new_posts = Post.order("posts.created_at DESC").limit(8)
 
     # 表示食材タグ名の表示
     @display_foods = Tag.where(display_food: true)
-    
+
     # 注目食材（管理者側）で設定したタグに関連する投稿の表示
     # 設定したタグの取得
     @pickup_foods = Tag.where(pickup_food: true)
     # タグに関連づく投稿の取得
+    @posts = []
     @pickup_foods.each do |pickup_food|
       @pickup_posts = pickup_food.posts.all
+      @posts += @pickup_posts
     end
+    # 複数タグで該当する投稿は一度だけ表示する
+    @posts = @posts.uniq
 
   end
-    
+
 end
